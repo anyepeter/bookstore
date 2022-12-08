a4f3a842f154364b7aefdc152fe06a8434829955
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-08 19:08:49 +0100

    add the caterory page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
-import Bookitems from './components/bookItem';
+import BookPage from './pages/home';
+import Categories from './pages/category';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Bookitems />
+      <Routes>
+        <Route exact path="/" element={<BookPage />} />
+        <Route exact path="/category" element={<Categories />} />
+      </Routes>
     </>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => (
       <ul>
         <li><Link to="/">Bookstore CMS</Link></li>
         <li><Link to="/">Books</Link></li>
-        <li><Link to="category">Category</Link></li>
+        <li><Link to="/category">Category</Link></li>
       </ul>
     </nav>
   </header>
