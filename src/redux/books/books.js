dcbccceb88863d282c9215baf22a738e41e31253
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-12 23:29:57 +0100

    create the book reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,10 +1,33 @@
 const ADDBOOK = 'ADDBOOK';
 const REMOVEBOOK = 'REMOVEBOOK';
 
-const addBook = () => {
+export const addBook = (payload) => {
   return {
     type: ADDBOOK,
-    id
+    paylaod
   }
 }
 
+export const removeBook = (id) => {
+  return {
+    type: REMOVEBOOK,
+    payload: id
+  }
+}
+
+const initialState = {
+   bookList: []
+}
+
+const bookListReducer = (state = initialState, action){
+  switch (action.type){
+    case ADDBOOK:
+      return [...state, action.payload]
+      case REMOVEBOOK:
+        return state.filter( book => book.id !== action.payload)
+        default:
+          return state
+  }
+}
+
+export default bookListReducer
