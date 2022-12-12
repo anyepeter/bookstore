0956fb9d8fa1037e8bb1cf06e9023a46cd9679ad
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-12 23:50:25 +0100

    create the category reducer

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,8 +1,21 @@
 const CHECKSTATUS = 'CHECKSTATUS';
 
-const checkStatus = (id) =>{
+export const checkStatus = (id) =>{
   return {
     type: CHECKSTATUS,
     id
+  }
+}
+
+const initialState = {
+  bookList: []
+}
+
+const categoryReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CHECKSTATUS:
+      return 'Under construction'
+      default:
+        return state
   }
 }
