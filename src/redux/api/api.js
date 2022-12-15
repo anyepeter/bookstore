85e0b43ae2d7c09e40b0f4c9e3b9d836c1d242de
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-15 21:08:33 +0100

    function to remove a book

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,12 +1,12 @@
 const apiLink = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/X8JnkuOPMOkJQ3tlKNbU/books/';
 
-const getBooks = async () => {
+export const getBooks = async () => {
 const getBookItems = await fetch(apiLink)
 .then((res) => res.json())
 return getBookItems
 }
 
-const addBooksItem = async (elements) => {
+export const addBooksItem = async (elements) => {
 const postBooks = await fetch(apiLink, {
   method: 'POST',
   headers: {
@@ -15,4 +15,11 @@ const postBooks = await fetch(apiLink, {
   body: JSON.stringify(elements),
 }).then((response) => response.text());
 return postBooks;
+}
+export const removeBookItem = async (id) => {
+  const removeBook = await fetch(`apilink${id}`, {
+      method: 'DELETE',
+    }).then((response) => response.text());
+    return removeBook;
+  
 }
