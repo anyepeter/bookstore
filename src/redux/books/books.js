99147f166ba1d2223b189961055ab792adb5481f
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-16 00:26:53 +0100

    refactor the reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -11,21 +11,35 @@ const GET_BOOK = 'bookstore/src/redux/books/GET_BOOK';
 
 export const removeBook = createAsyncThunk(REMOVE_BOOK, removeBookItem);
 
+const initialState = {
+  books: [],
+  status: null,
+}
 
+const bookReducerSlice = createSlice({
+name: 'books',
+initialState,  
+extraReducers: (builder) => {
+  builder.addCase(fetchBooks.fulfilled, (state, action) => {
+    const items = state;
+    items.status = 'successful';
+    items.books = action.payload;
+  }),
 
-const bookListReducer = (state = items, action = {}) => {
-  switch (action.type) {
-    case ADD_BOOK:
-      return [...state,
-        action.payload,
-      ];
-    case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.id);
-    default:
-      return state;
-  }
-};
+  builder.addCase(removeBook.fulfilled, (state, action) => {
+    const items = state;
+    state.status = 'successful';
+    items.books = state.filter((book) => book.id !== action.id);
+  }),
 
+  builder.addCase(addBook.fulfilled, (state, action) => {
+    state.status = 'successful'
+    state.books = [
+      ...state.books,
+      action.payload,
+    ]
+  })
+}
+})
 
-
-export default bookListReducer;
+export default bookReducerSlice.reducer;
